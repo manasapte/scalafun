3f9b94809b1d32097a32ec583941489fd4060a4a
Manas Apte <@onekingslane.com>
2013-07-08 17:12:27 -0700

    removing all worksheet artifacts

--- progfun/.worksheet/src/week3.rationals2.scala
+++
@@ -1,6 +0,0 @@
-package week3
-
-object rationals2 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(78); 
-  println("Welcome to the Scala worksheet")}
-  
-}
--- progfun/.worksheet/src/week4.Boolean.scala
+++
@@ -1,6 +0,0 @@
-package week4
-
-object Boolean {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(75); 
-  println("Welcome to the Scala worksheet")}
-}
-package idealized.scala
