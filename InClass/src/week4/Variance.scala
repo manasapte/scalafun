428adeea7fc67122fdff03e4807a9d378dff815f
Manas Apte <manasapte@gmail.com>
2013-11-28 17:50:48 -0800

    committing

---
+++ InClass/src/week4/Variance.scala
@@ -0,0 +1,61 @@
+package week4
+import scala.Boolean
+object Variance extends App {
+/**
+ * Typing rules for functions:
+ * We have the following rule for the subtyping between the function types:
+ * If A2 <: A1 and B1 <: B2, then
+ * A1 => B1 <: A2 => B2
+ * So function types are 
+ *     covariant in the return types
+ * AND contravariant in argument types 
+ */
+ // so we can define the function trait again as follows
+  trait Function1[-T, +U] {
+    def apply(t: T): U
+  }
+/**
+ *  Now let's redefine our cons list using the concept of variance.
+ *  one short coming here is the Nil type is defined as a class instead of an object.
+ */ 
+  trait List[+T] {
+    def isEmpty: scala.Boolean
+    def head: T
+    def tail: List[T]
+    // def prepend(elem: T): List[T] = new Cons(elem, this) ---> does not work.
+    def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
+  }
+ 
+  class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+    def isEmpty: scala.Boolean = false
+  }
+  
+  object Nil extends List[Nothing] {
+    def isEmpty: scala.Boolean = true
+    def head: Nothing = throw new NoSuchElementException("Nil.head")
+    def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+  }
+  
+  object test {
+    // Nothing is a subtype of string, and we have defined List to be covariant
+    // therefore list of Nothing is a subtype of list of String
+    val x: List[String] = Nil
+    /*
+     * some times we have to put in some work to make a type covariant.
+     * Let's add a prepend operation to list.
+     * prepend[T](elem: T) = new Cons(elem, this)
+     * Since list is covariant in T, it can not accept T as a function argument.
+     * Here's the problem
+     * var xs: List[IntSet] = new ...
+     * xs.prepend(Empty)
+     * but then on 
+     * var ys: List[NonEmpty] = new ...
+     * ys.prepend(Empty) -- Fails and therefore, list is not covariant any more.
+     * to solve this we can use some super type of T
+     * So def prepend[U >: T](elem: T): List[U] = new Cons(elem, this) 
+     */
+     // what is the return type of the following:
+    def f(xs: List[NonEmpty], y: Empty) = xs.prepend(y)
+  }
+  
+}
