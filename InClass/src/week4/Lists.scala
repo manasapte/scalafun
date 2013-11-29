10dab9bfe5ffe47e13a515c641138331803645ff
Manas Apte <manasapte@gmail.com>
2013-11-29 13:19:00 -0800

    committing

--- InClass/src/week4/Lists.scala
+++ InClass/src/week4/Lists.scala
@@ -1,5 +1,16 @@
 package week4
+import scala.List
 
 object Lists {
-
+  /**
+   * Let's sort lists using list pattern matching and insertion sort algorithm.
+   */
+  def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+    case List() => List(x)
+    case h :: t => if(x <= h) x :: xs else h :: insert(x, t)
+  }
+  def isort(xs: List[Int]): List[Int] = xs match {
+    case List() => List()
+    case h :: t => insert(h, isort(t))
+  }
 }
