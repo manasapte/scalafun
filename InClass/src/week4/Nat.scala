6cb957116f8c784ab8d660cfb584cebb5ee50744
Manas Apte <manasapte@gmail.com>
2013-11-28 17:51:47 -0800

    committing nat

--- InClass/src/week4/Nat.scala
+++ InClass/src/week4/Nat.scala
@@ -1,7 +1,6 @@
 package week4
-
 abstract class Nat {
-  def isZero: Boolean
+  def isZero: scala.Boolean
   def predecessor: Nat
   def succesor: Nat
   def +(that: Nat): Nat
@@ -9,7 +8,7 @@ abstract class Nat {
 }
 
 object Zero extends Nat {
-  def isZero: Boolean = True
+  def isZero: scala.Boolean = true
   def predecessor: Nat = throw new Error("0.predecessor")
   def succesor: Nat = new Succ(this) 
   def +(that: Nat): Nat = that
@@ -17,7 +16,7 @@ object Zero extends Nat {
 }
 
 class Succ(val predecessor: Nat) extends Nat {
-  def isZero: Boolean = False
+  def isZero: scala.Boolean = false
   def succesor: Nat = new Succ(this) 
   def +(that: Nat): Nat = predecessor + new Succ(that)
   def -(that: Nat): Nat = if(that.isZero) this else predecessor - that.predecessor
