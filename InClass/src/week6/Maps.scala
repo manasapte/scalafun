7e19fb610bbf7109669abc0b4f41ce62e5416c32
Manas Apte <manasapte@gmail.com>
2013-11-30 20:28:02 -0800

    committing maps

---
+++ InClass/src/week6/Maps.scala
@@ -0,0 +1,26 @@
+package week6
+
+object Maps extends App {
+  // represents polynomials as maps
+  class Poly(terms0: Map[Int, Double]) {
+    def this(bindings: (Int, Double)*) = this(bindings.toMap)
+    val terms = terms0 withDefaultValue 0.0
+    
+    def +(other: Poly): Poly = new Poly((other.terms foldLeft terms)(addTerm))
+    
+    def addTerm(terms: Map[Int, Double], term: (Int, Double)) = {
+      val (exp, coeff) = term
+      terms + (exp -> (coeff + terms(exp)))
+    }
+      
+    
+    override def toString =
+      (for((exp, coeff) <- terms.toList.sorted.reverse) yield coeff + "x^" +exp).mkString(" + ")
+  }
+  
+  val p1 = new Poly(Map(1 -> 2.0, 3 -> 4.0, 5 -> 6.2))
+  val p2 = new Poly(Map(0 -> 3.0, 3 -> 7.0))
+  println(p1)
+  println(p2)
+  println(p1 + p2)
+}
